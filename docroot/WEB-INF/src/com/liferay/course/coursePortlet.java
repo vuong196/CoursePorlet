a65d139f94f6d17ec8ce4f5712673ad3fb8c43bd
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-07-29 15:42:08 +0700

    CM_PORLET_COURSE: Create add, edit, delete for Course Porlet

--- docroot/WEB-INF/src/com/liferay/course/coursePortlet.java
+++ docroot/WEB-INF/src/com/liferay/course/coursePortlet.java
@@ -1,12 +1,63 @@
+
 package com.liferay.course;
 
-import com.liferay.util.bridges.mvc.MVCPortlet;
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.course.model.Course;
+import com.liferay.course.service.CourseLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
  * Portlet implementation class coursePortlet
  */
 public class coursePortlet extends MVCPortlet {
-	
 
+    public void addCourse(final ActionRequest actionRequest, final ActionResponse actionResponse)
+	    throws SystemException {
+
+	final String name = ParamUtil.getString(actionRequest, "name");
+	final String description = ParamUtil.getString(actionRequest, "description");
+	final String lecturer = ParamUtil.getString(actionRequest, "lecturer");
+	final int duration = ParamUtil.getInteger(actionRequest, "duration");
+	final boolean status = ParamUtil.getBoolean(actionRequest, "status");
+
+	final Course course = CourseLocalServiceUtil.createCourse(CounterLocalServiceUtil.increment());
+	course.setName(name);
+	course.setDescription(description);
+	course.setLecturer(lecturer);
+	course.setDuration(duration);
+	course.setStatus(status);
+
+	CourseLocalServiceUtil.addCourse(course);
+    }
+
+    public void deleteCourse(final ActionRequest actionRequest, final ActionResponse actionResponse)
+	    throws SystemException, PortalException {
+
+	CourseLocalServiceUtil.deleteCourse(ParamUtil.getLong(actionRequest, "courseId"));
+    }
+
+    public void editCourse(final ActionRequest actionRequest, final ActionResponse actionResponse)
+	    throws SystemException, PortalException {
+
+	final String name = ParamUtil.getString(actionRequest, "name");
+	final String description = ParamUtil.getString(actionRequest, "description");
+	final String lecturer = ParamUtil.getString(actionRequest, "lecturer");
+	final int duration = ParamUtil.getInteger(actionRequest, "duration");
+	final boolean status = ParamUtil.getBoolean(actionRequest, "status");
+
+	final Course course = CourseLocalServiceUtil.fetchCourse(ParamUtil.getInteger(actionRequest, "id"));
+	course.setName(name);
+	course.setDescription(description);
+	course.setLecturer(lecturer);
+	course.setDuration(duration);
+	course.setStatus(status);
+
+	CourseLocalServiceUtil.updateCourse(course);
+    }
 }
