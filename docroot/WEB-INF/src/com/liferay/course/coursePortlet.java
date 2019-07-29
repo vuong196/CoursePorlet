43951d493e65af6d3534d28a35aef9f8390babb7
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-07-29 14:52:05 +0700

    CM_PORLET: Create course porlet

---
+++ docroot/WEB-INF/src/com/liferay/course/coursePortlet.java
@@ -0,0 +1,12 @@
+package com.liferay.course;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+
+/**
+ * Portlet implementation class coursePortlet
+ */
+public class coursePortlet extends MVCPortlet {
+	
+
+}
---
+++ docroot/html/course/view.jsp
@@ -0,0 +1,8 @@
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+
+<portlet:defineObjects />
+
+This is the <b>Course</b> portlet in View mode.
+
+
