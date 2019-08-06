050ae5b71be3d944a959a10951be0833bbf61d01
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-08-06 15:40:41 +0700

    CM_Course: Create CRUD functions using CourseServiceUtil in CourseListPorlet

--- docroot/WEB-INF/src/com/liferay/course/CourseListPortlet.java
+++ docroot/WEB-INF/src/com/liferay/course/CourseListPortlet.java
@@ -1,11 +1,60 @@
+
 package com.liferay.course;
 
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import com.liferay.course.model.Course;
+import com.liferay.course.service.CourseServiceUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
  * Portlet implementation class CourseListPortlet
  */
 public class CourseListPortlet extends MVCPortlet {
- 
 
+	public void addCourse(final ActionRequest actionRequest, final ActionResponse actionResponse) throws Exception {
+
+		String name = ParamUtil.getString(actionRequest, "name");
+		String description = ParamUtil.getString(actionRequest, "description");
+		String lecturer = ParamUtil.getString(actionRequest, "lecturer");
+		int duration = ParamUtil.getInteger(actionRequest, "duration");
+		boolean status = ParamUtil.getBoolean(actionRequest, "status");
+
+		CourseServiceUtil.addCourse(name, description, lecturer, duration, status);
+	}
+
+	public void deleteCourse(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+
+		long id = ParamUtil.getLong(actionRequest, "courseId");
+
+		CourseServiceUtil.deleteCourse(id);
+	}
+
+	public List<Course> getAllCourses(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+
+		return CourseServiceUtil.getAllCourses();
+	}
+
+	public Course getCourseById(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+
+		long id = ParamUtil.getLong(actionRequest, "courseId");
+
+		return CourseServiceUtil.getCourseById(id);
+	}
+
+	public void updateCourse(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+
+		long id = ParamUtil.getLong(actionRequest, "courseId");
+		String name = ParamUtil.getString(actionRequest, "name");
+		String description = ParamUtil.getString(actionRequest, "description");
+		String lecturer = ParamUtil.getString(actionRequest, "lecturer");
+		int duration = ParamUtil.getInteger(actionRequest, "duration");
+		boolean status = ParamUtil.getBoolean(actionRequest, "status");
+
+		CourseServiceUtil.updateCourse(id, name, description, lecturer, duration, status);
+	}
 }
