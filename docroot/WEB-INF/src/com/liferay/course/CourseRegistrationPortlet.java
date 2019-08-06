9f6775909c4e00e1a445caf0c02e591b1a2177b0
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-08-06 17:54:14 +0700

    CM-Porlet: Create a new porlet - course-registration-porlet

---
+++ docroot/WEB-INF/src/com/liferay/course/CourseRegistrationPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.course;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class CourseRegistrationPortlet
+ */
+public class CourseRegistrationPortlet extends MVCPortlet {
+ 
+
+}
