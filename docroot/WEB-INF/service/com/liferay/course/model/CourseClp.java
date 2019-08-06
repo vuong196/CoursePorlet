2af72c6153707e0003d3c87d776895d4d9f095ea
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-08-06 14:30:24 +0700

    CM_Course_Service: Run build service

---
+++ docroot/WEB-INF/service/com/liferay/course/model/CourseClp.java
@@ -0,0 +1,462 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.course.model;
+
+import com.liferay.course.service.ClpSerializer;
+import com.liferay.course.service.CourseLocalServiceUtil;
+
+import com.liferay.portal.kernel.bean.AutoEscapeBeanHandler;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.model.impl.BaseModelImpl;
+
+import java.io.Serializable;
+
+import java.lang.reflect.Method;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ces
+ */
+public class CourseClp extends BaseModelImpl<Course> implements Course {
+	public CourseClp() {
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		return Course.class;
+	}
+
+	@Override
+	public String getModelClassName() {
+		return Course.class.getName();
+	}
+
+	@Override
+	public long getPrimaryKey() {
+		return _courseId;
+	}
+
+	@Override
+	public void setPrimaryKey(long primaryKey) {
+		setCourseId(primaryKey);
+	}
+
+	@Override
+	public Serializable getPrimaryKeyObj() {
+		return _courseId;
+	}
+
+	@Override
+	public void setPrimaryKeyObj(Serializable primaryKeyObj) {
+		setPrimaryKey(((Long)primaryKeyObj).longValue());
+	}
+
+	@Override
+	public Map<String, Object> getModelAttributes() {
+		Map<String, Object> attributes = new HashMap<String, Object>();
+
+		attributes.put("courseId", getCourseId());
+		attributes.put("name", getName());
+		attributes.put("description", getDescription());
+		attributes.put("lecturer", getLecturer());
+		attributes.put("duration", getDuration());
+		attributes.put("status", getStatus());
+
+		return attributes;
+	}
+
+	@Override
+	public void setModelAttributes(Map<String, Object> attributes) {
+		Long courseId = (Long)attributes.get("courseId");
+
+		if (courseId != null) {
+			setCourseId(courseId);
+		}
+
+		String name = (String)attributes.get("name");
+
+		if (name != null) {
+			setName(name);
+		}
+
+		String description = (String)attributes.get("description");
+
+		if (description != null) {
+			setDescription(description);
+		}
+
+		String lecturer = (String)attributes.get("lecturer");
+
+		if (lecturer != null) {
+			setLecturer(lecturer);
+		}
+
+		Integer duration = (Integer)attributes.get("duration");
+
+		if (duration != null) {
+			setDuration(duration);
+		}
+
+		Boolean status = (Boolean)attributes.get("status");
+
+		if (status != null) {
+			setStatus(status);
+		}
+	}
+
+	@Override
+	public long getCourseId() {
+		return _courseId;
+	}
+
+	@Override
+	public void setCourseId(long courseId) {
+		_courseId = courseId;
+
+		if (_courseRemoteModel != null) {
+			try {
+				Class<?> clazz = _courseRemoteModel.getClass();
+
+				Method method = clazz.getMethod("setCourseId", long.class);
+
+				method.invoke(_courseRemoteModel, courseId);
+			}
+			catch (Exception e) {
+				throw new UnsupportedOperationException(e);
+			}
+		}
+	}
+
+	@Override
+	public String getName() {
+		return _name;
+	}
+
+	@Override
+	public void setName(String name) {
+		_name = name;
+
+		if (_courseRemoteModel != null) {
+			try {
+				Class<?> clazz = _courseRemoteModel.getClass();
+
+				Method method = clazz.getMethod("setName", String.class);
+
+				method.invoke(_courseRemoteModel, name);
+			}
+			catch (Exception e) {
+				throw new UnsupportedOperationException(e);
+			}
+		}
+	}
+
+	@Override
+	public String getDescription() {
+		return _description;
+	}
+
+	@Override
+	public void setDescription(String description) {
+		_description = description;
+
+		if (_courseRemoteModel != null) {
+			try {
+				Class<?> clazz = _courseRemoteModel.getClass();
+
+				Method method = clazz.getMethod("setDescription", String.class);
+
+				method.invoke(_courseRemoteModel, description);
+			}
+			catch (Exception e) {
+				throw new UnsupportedOperationException(e);
+			}
+		}
+	}
+
+	@Override
+	public String getLecturer() {
+		return _lecturer;
+	}
+
+	@Override
+	public void setLecturer(String lecturer) {
+		_lecturer = lecturer;
+
+		if (_courseRemoteModel != null) {
+			try {
+				Class<?> clazz = _courseRemoteModel.getClass();
+
+				Method method = clazz.getMethod("setLecturer", String.class);
+
+				method.invoke(_courseRemoteModel, lecturer);
+			}
+			catch (Exception e) {
+				throw new UnsupportedOperationException(e);
+			}
+		}
+	}
+
+	@Override
+	public int getDuration() {
+		return _duration;
+	}
+
+	@Override
+	public void setDuration(int duration) {
+		_duration = duration;
+
+		if (_courseRemoteModel != null) {
+			try {
+				Class<?> clazz = _courseRemoteModel.getClass();
+
+				Method method = clazz.getMethod("setDuration", int.class);
+
+				method.invoke(_courseRemoteModel, duration);
+			}
+			catch (Exception e) {
+				throw new UnsupportedOperationException(e);
+			}
+		}
+	}
+
+	@Override
+	public boolean getStatus() {
+		return _status;
+	}
+
+	@Override
+	public boolean isStatus() {
+		return _status;
+	}
+
+	@Override
+	public void setStatus(boolean status) {
+		_status = status;
+
+		if (_courseRemoteModel != null) {
+			try {
+				Class<?> clazz = _courseRemoteModel.getClass();
+
+				Method method = clazz.getMethod("setStatus", boolean.class);
+
+				method.invoke(_courseRemoteModel, status);
+			}
+			catch (Exception e) {
+				throw new UnsupportedOperationException(e);
+			}
+		}
+	}
+
+	public BaseModel<?> getCourseRemoteModel() {
+		return _courseRemoteModel;
+	}
+
+	public void setCourseRemoteModel(BaseModel<?> courseRemoteModel) {
+		_courseRemoteModel = courseRemoteModel;
+	}
+
+	public Object invokeOnRemoteModel(String methodName,
+		Class<?>[] parameterTypes, Object[] parameterValues)
+		throws Exception {
+		Object[] remoteParameterValues = new Object[parameterValues.length];
+
+		for (int i = 0; i < parameterValues.length; i++) {
+			if (parameterValues[i] != null) {
+				remoteParameterValues[i] = ClpSerializer.translateInput(parameterValues[i]);
+			}
+		}
+
+		Class<?> remoteModelClass = _courseRemoteModel.getClass();
+
+		ClassLoader remoteModelClassLoader = remoteModelClass.getClassLoader();
+
+		Class<?>[] remoteParameterTypes = new Class[parameterTypes.length];
+
+		for (int i = 0; i < parameterTypes.length; i++) {
+			if (parameterTypes[i].isPrimitive()) {
+				remoteParameterTypes[i] = parameterTypes[i];
+			}
+			else {
+				String parameterTypeName = parameterTypes[i].getName();
+
+				remoteParameterTypes[i] = remoteModelClassLoader.loadClass(parameterTypeName);
+			}
+		}
+
+		Method method = remoteModelClass.getMethod(methodName,
+				remoteParameterTypes);
+
+		Object returnValue = method.invoke(_courseRemoteModel,
+				remoteParameterValues);
+
+		if (returnValue != null) {
+			returnValue = ClpSerializer.translateOutput(returnValue);
+		}
+
+		return returnValue;
+	}
+
+	@Override
+	public void persist() throws SystemException {
+		if (this.isNew()) {
+			CourseLocalServiceUtil.addCourse(this);
+		}
+		else {
+			CourseLocalServiceUtil.updateCourse(this);
+		}
+	}
+
+	@Override
+	public Course toEscapedModel() {
+		return (Course)ProxyUtil.newProxyInstance(Course.class.getClassLoader(),
+			new Class[] { Course.class }, new AutoEscapeBeanHandler(this));
+	}
+
+	@Override
+	public Object clone() {
+		CourseClp clone = new CourseClp();
+
+		clone.setCourseId(getCourseId());
+		clone.setName(getName());
+		clone.setDescription(getDescription());
+		clone.setLecturer(getLecturer());
+		clone.setDuration(getDuration());
+		clone.setStatus(getStatus());
+
+		return clone;
+	}
+
+	@Override
+	public int compareTo(Course course) {
+		int value = 0;
+
+		if (getCourseId() < course.getCourseId()) {
+			value = -1;
+		}
+		else if (getCourseId() > course.getCourseId()) {
+			value = 1;
+		}
+		else {
+			value = 0;
+		}
+
+		value = value * -1;
+
+		if (value != 0) {
+			return value;
+		}
+
+		return 0;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof CourseClp)) {
+			return false;
+		}
+
+		CourseClp course = (CourseClp)obj;
+
+		long primaryKey = course.getPrimaryKey();
+
+		if (getPrimaryKey() == primaryKey) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return (int)getPrimaryKey();
+	}
+
+	@Override
+	public String toString() {
+		StringBundler sb = new StringBundler(13);
+
+		sb.append("{courseId=");
+		sb.append(getCourseId());
+		sb.append(", name=");
+		sb.append(getName());
+		sb.append(", description=");
+		sb.append(getDescription());
+		sb.append(", lecturer=");
+		sb.append(getLecturer());
+		sb.append(", duration=");
+		sb.append(getDuration());
+		sb.append(", status=");
+		sb.append(getStatus());
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	@Override
+	public String toXmlString() {
+		StringBundler sb = new StringBundler(22);
+
+		sb.append("<model><model-name>");
+		sb.append("com.liferay.course.model.Course");
+		sb.append("</model-name>");
+
+		sb.append(
+			"<column><column-name>courseId</column-name><column-value><![CDATA[");
+		sb.append(getCourseId());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>name</column-name><column-value><![CDATA[");
+		sb.append(getName());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>description</column-name><column-value><![CDATA[");
+		sb.append(getDescription());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>lecturer</column-name><column-value><![CDATA[");
+		sb.append(getLecturer());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>duration</column-name><column-value><![CDATA[");
+		sb.append(getDuration());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>status</column-name><column-value><![CDATA[");
+		sb.append(getStatus());
+		sb.append("]]></column-value></column>");
+
+		sb.append("</model>");
+
+		return sb.toString();
+	}
+
+	private long _courseId;
+	private String _name;
+	private String _description;
+	private String _lecturer;
+	private int _duration;
+	private boolean _status;
+	private BaseModel<?> _courseRemoteModel;
+}
---
+++ docroot/WEB-INF/service/com/liferay/course/model/CourseWrapper.java
@@ -0,0 +1,391 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.course.model;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.ModelWrapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ * This class is a wrapper for {@link Course}.
+ * </p>
+ *
+ * @author ces
+ * @see Course
+ * @generated
+ */
+public class CourseWrapper implements Course, ModelWrapper<Course> {
+	public CourseWrapper(Course course) {
+		_course = course;
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		return Course.class;
+	}
+
+	@Override
+	public String getModelClassName() {
+		return Course.class.getName();
+	}
+
+	@Override
+	public Map<String, Object> getModelAttributes() {
+		Map<String, Object> attributes = new HashMap<String, Object>();
+
+		attributes.put("courseId", getCourseId());
+		attributes.put("name", getName());
+		attributes.put("description", getDescription());
+		attributes.put("lecturer", getLecturer());
+		attributes.put("duration", getDuration());
+		attributes.put("status", getStatus());
+
+		return attributes;
+	}
+
+	@Override
+	public void setModelAttributes(Map<String, Object> attributes) {
+		Long courseId = (Long)attributes.get("courseId");
+
+		if (courseId != null) {
+			setCourseId(courseId);
+		}
+
+		String name = (String)attributes.get("name");
+
+		if (name != null) {
+			setName(name);
+		}
+
+		String description = (String)attributes.get("description");
+
+		if (description != null) {
+			setDescription(description);
+		}
+
+		String lecturer = (String)attributes.get("lecturer");
+
+		if (lecturer != null) {
+			setLecturer(lecturer);
+		}
+
+		Integer duration = (Integer)attributes.get("duration");
+
+		if (duration != null) {
+			setDuration(duration);
+		}
+
+		Boolean status = (Boolean)attributes.get("status");
+
+		if (status != null) {
+			setStatus(status);
+		}
+	}
+
+	/**
+	* Returns the primary key of this course.
+	*
+	* @return the primary key of this course
+	*/
+	@Override
+	public long getPrimaryKey() {
+		return _course.getPrimaryKey();
+	}
+
+	/**
+	* Sets the primary key of this course.
+	*
+	* @param primaryKey the primary key of this course
+	*/
+	@Override
+	public void setPrimaryKey(long primaryKey) {
+		_course.setPrimaryKey(primaryKey);
+	}
+
+	/**
+	* Returns the course ID of this course.
+	*
+	* @return the course ID of this course
+	*/
+	@Override
+	public long getCourseId() {
+		return _course.getCourseId();
+	}
+
+	/**
+	* Sets the course ID of this course.
+	*
+	* @param courseId the course ID of this course
+	*/
+	@Override
+	public void setCourseId(long courseId) {
+		_course.setCourseId(courseId);
+	}
+
+	/**
+	* Returns the name of this course.
+	*
+	* @return the name of this course
+	*/
+	@Override
+	public java.lang.String getName() {
+		return _course.getName();
+	}
+
+	/**
+	* Sets the name of this course.
+	*
+	* @param name the name of this course
+	*/
+	@Override
+	public void setName(java.lang.String name) {
+		_course.setName(name);
+	}
+
+	/**
+	* Returns the description of this course.
+	*
+	* @return the description of this course
+	*/
+	@Override
+	public java.lang.String getDescription() {
+		return _course.getDescription();
+	}
+
+	/**
+	* Sets the description of this course.
+	*
+	* @param description the description of this course
+	*/
+	@Override
+	public void setDescription(java.lang.String description) {
+		_course.setDescription(description);
+	}
+
+	/**
+	* Returns the lecturer of this course.
+	*
+	* @return the lecturer of this course
+	*/
+	@Override
+	public java.lang.String getLecturer() {
+		return _course.getLecturer();
+	}
+
+	/**
+	* Sets the lecturer of this course.
+	*
+	* @param lecturer the lecturer of this course
+	*/
+	@Override
+	public void setLecturer(java.lang.String lecturer) {
+		_course.setLecturer(lecturer);
+	}
+
+	/**
+	* Returns the duration of this course.
+	*
+	* @return the duration of this course
+	*/
+	@Override
+	public int getDuration() {
+		return _course.getDuration();
+	}
+
+	/**
+	* Sets the duration of this course.
+	*
+	* @param duration the duration of this course
+	*/
+	@Override
+	public void setDuration(int duration) {
+		_course.setDuration(duration);
+	}
+
+	/**
+	* Returns the status of this course.
+	*
+	* @return the status of this course
+	*/
+	@Override
+	public boolean getStatus() {
+		return _course.getStatus();
+	}
+
+	/**
+	* Returns <code>true</code> if this course is status.
+	*
+	* @return <code>true</code> if this course is status; <code>false</code> otherwise
+	*/
+	@Override
+	public boolean isStatus() {
+		return _course.isStatus();
+	}
+
+	/**
+	* Sets whether this course is status.
+	*
+	* @param status the status of this course
+	*/
+	@Override
+	public void setStatus(boolean status) {
+		_course.setStatus(status);
+	}
+
+	@Override
+	public boolean isNew() {
+		return _course.isNew();
+	}
+
+	@Override
+	public void setNew(boolean n) {
+		_course.setNew(n);
+	}
+
+	@Override
+	public boolean isCachedModel() {
+		return _course.isCachedModel();
+	}
+
+	@Override
+	public void setCachedModel(boolean cachedModel) {
+		_course.setCachedModel(cachedModel);
+	}
+
+	@Override
+	public boolean isEscapedModel() {
+		return _course.isEscapedModel();
+	}
+
+	@Override
+	public java.io.Serializable getPrimaryKeyObj() {
+		return _course.getPrimaryKeyObj();
+	}
+
+	@Override
+	public void setPrimaryKeyObj(java.io.Serializable primaryKeyObj) {
+		_course.setPrimaryKeyObj(primaryKeyObj);
+	}
+
+	@Override
+	public com.liferay.portlet.expando.model.ExpandoBridge getExpandoBridge() {
+		return _course.getExpandoBridge();
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(
+		com.liferay.portal.model.BaseModel<?> baseModel) {
+		_course.setExpandoBridgeAttributes(baseModel);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(
+		com.liferay.portlet.expando.model.ExpandoBridge expandoBridge) {
+		_course.setExpandoBridgeAttributes(expandoBridge);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(
+		com.liferay.portal.service.ServiceContext serviceContext) {
+		_course.setExpandoBridgeAttributes(serviceContext);
+	}
+
+	@Override
+	public java.lang.Object clone() {
+		return new CourseWrapper((Course)_course.clone());
+	}
+
+	@Override
+	public int compareTo(Course course) {
+		return _course.compareTo(course);
+	}
+
+	@Override
+	public int hashCode() {
+		return _course.hashCode();
+	}
+
+	@Override
+	public com.liferay.portal.model.CacheModel<Course> toCacheModel() {
+		return _course.toCacheModel();
+	}
+
+	@Override
+	public Course toEscapedModel() {
+		return new CourseWrapper(_course.toEscapedModel());
+	}
+
+	@Override
+	public Course toUnescapedModel() {
+		return new CourseWrapper(_course.toUnescapedModel());
+	}
+
+	@Override
+	public java.lang.String toString() {
+		return _course.toString();
+	}
+
+	@Override
+	public java.lang.String toXmlString() {
+		return _course.toXmlString();
+	}
+
+	@Override
+	public void persist()
+		throws com.liferay.portal.kernel.exception.SystemException {
+		_course.persist();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof CourseWrapper)) {
+			return false;
+		}
+
+		CourseWrapper courseWrapper = (CourseWrapper)obj;
+
+		if (Validator.equals(_course, courseWrapper._course)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #getWrappedModel}
+	 */
+	public Course getWrappedCourse() {
+		return _course;
+	}
+
+	@Override
+	public Course getWrappedModel() {
+		return _course;
+	}
+
+	@Override
+	public void resetOriginalValues() {
+		_course.resetOriginalValues();
+	}
+
+	private Course _course;
+}
