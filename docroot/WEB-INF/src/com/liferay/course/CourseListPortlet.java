98007e5e95622681c673a92ee396531a85b4b62a
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-08-06 14:12:25 +0700

    CM_Porlet: Create a new porlet - Course List to manage existing courses

---
+++ docroot/WEB-INF/src/com/liferay/course/CourseListPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.course;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class CourseListPortlet
+ */
+public class CourseListPortlet extends MVCPortlet {
+ 
+
+}
---
+++ docroot/html/courselist/view.jsp
@@ -0,0 +1,5 @@
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<portlet:defineObjects />
+
+This is the <b>Course List</b> portlet in View mode.
