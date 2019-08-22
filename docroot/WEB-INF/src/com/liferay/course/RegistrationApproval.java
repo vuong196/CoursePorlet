a5b2ac6643608e6ff8d0ddff4e50106cd0060cba
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-08-22 14:18:26 +0700

    CM_Portlet-RegistrationApproval-Controller: Add CRUD functions with permissions for RegistrationApproval

--- docroot/WEB-INF/src/com/liferay/course/RegistrationApproval.java
+++ docroot/WEB-INF/src/com/liferay/course/RegistrationApproval.java
@@ -1,6 +1,15 @@
 
 package com.liferay.course;
 
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import com.liferay.course.model.CourseRegistration;
+import com.liferay.course.service.CourseRegistrationServiceUtil;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
@@ -8,4 +17,29 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
  */
 public class RegistrationApproval extends MVCPortlet {
 
+	public void approveCourseRegistration(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+
+		long courseId = ParamUtil.getLong(actionRequest, "courseId");
+
+		List<CourseRegistration> c = CourseRegistrationServiceUtil
+			.getCourseRegistrationByCourseIdAndStatus(courseId, 0);
+
+		if (c.size() < 20) {
+
+			long id = ParamUtil.getLong(actionRequest, "id");
+
+			CourseRegistrationServiceUtil.updateStatusOfCourseRegistration(id, 1);
+		}
+		else {
+
+			SessionErrors.add(actionRequest, "error");
+		}
+	}
+
+	public void rejectCourseRegistration(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+
+		long id = ParamUtil.getLong(actionRequest, "id");
+
+		CourseRegistrationServiceUtil.updateStatusOfCourseRegistration(id, -1);
+	}
 }
