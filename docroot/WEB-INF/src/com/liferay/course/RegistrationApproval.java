6c8e4553b7a6631a014e5d87e31aba32acccb030
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-08-22 14:10:13 +0700

    CM_Portlet: Create a new portet - RegistrationApproval

---
+++ docroot/WEB-INF/src/com/liferay/course/RegistrationApproval.java
@@ -0,0 +1,11 @@
+
+package com.liferay.course;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class RegistrationApproval
+ */
+public class RegistrationApproval extends MVCPortlet {
+
+}
