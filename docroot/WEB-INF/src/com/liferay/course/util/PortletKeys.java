12b867152aa701598ed9ee71bbaa239466770cb0
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-08-30 15:30:15 +0700

    CM_Porlet-Course-Util: Add some ActionKeys and PortletKeys

---
+++ docroot/WEB-INF/src/com/liferay/course/util/PortletKeys.java
@@ -0,0 +1,9 @@
+
+package com.liferay.course.util;
+
+public class PortletKeys extends com.liferay.portal.util.PortletKeys {
+
+	public static final String COURSE = "course_WAR_courseportlet";
+
+	public static final String COURSE_ADMIN = "courseadmin_WAR_courseportlet";
+}
